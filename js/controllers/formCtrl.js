deee2a49f677165523237b3611efb4d89a5587d6
mreziko <abedmdz28@gmail.com>
2016-09-27 16:16:20 -0500

    4th change:

--- js/controllers/formCtrl.js
+++ js/controllers/formCtrl.js
@@ -1,11 +1,12 @@
 /**
  * Created by Reziko on 9/27/2016.
  */
-angular.module("person_form").controller("formCtrl",["$scope", "serviceRec", function($scope, serviceRec) {
+angular.module("person_form").controller("formCtrl",["$scope", "serviceRec",
+    function($scope, serviceRec) {
 
-    $scope.personInfo = "";
-
+   // $scope.personInfo = "";
     $scope.$watch(function () {
+
         return serviceRec.personInfo;
     }, 
     function (newVal, oldVal) {
@@ -20,4 +21,22 @@ angular.module("person_form").controller("formCtrl",["$scope", "serviceRec", fun
         }
 
     });
+
+
+    $scope.$watch(function () {
+        return serviceRec.personInfoIds;
+
+    },
+    function (newVal, oldVal) {
+        if(oldVal != newVal){
+            console.log("From Watch ,Person Table");
+            console.log(newVal);
+            console.log("OLD VAL Person Table");
+            console.log(oldVal);
+
+            $scope.personInfoIds;
+        }
+    });
+
+
 }]);
--- js/services/serviceRec.js
+++ js/services/serviceRec.js
@@ -2,13 +2,21 @@
  * Created by Reziko on 9/27/2016.
  */
 
-angular.module('person_form').service('servcieRec',["$http", function ($http) {
+angular.module('person_form').service('serviceRec',["$http", function ($http) {
     var obj = this;
 
     $http.get("/service/person_table").then(function (response) {
 
-        console.log("Logging persons")
+        console.log("Logging Persons Table");
         obj.personInfo = response.data;
         console.log(obj.personInfo);
     });
+
+    this.personInfoIds = null;
+
+    $http.get('/service/person_table/2').then(function (response) {
+        console.log("Logging Persons Table");
+        obj.personInfoIds = response.data;
+        console.log(obj.personInfoIds)
+    })
 }]);
