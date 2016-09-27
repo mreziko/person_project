60dc05e489b8e4e619c95f79f16ee6dc6081dbf8
mreziko <abedmdz28@gmail.com>
2016-09-27 12:05:34 -0500

    2nd up date

---
+++ js/person_form.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Reziko on 9/27/2016.
+ */
+angular.module("person_form",[]);
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+
+
+
+
+
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var connection = require('express-myconnection');
@@ -20,24 +24,24 @@ app.use(connection(mysql,{
     host        : 'localhost',
     user        : 'ui',
     password    : 'ui1234',
-    database    : 'information_cart'
+    database    : 'information_cart',
     },'request'));
 
 //Routing
 //this is routing but to connect the rest API with our database
 
-var url = "/service/person_table";
 
 
-app.get(url,function(req, res, next){
+app.get("/service/person_table",function(req, res, next){
    //arrays to store dynamic parameters
-    
+
+    var ids=[];
     
     var query = "SELECT * FROM person_table";
     req.getConnection(function(err, connection){
        if(err) return next(err);
         
-        connection.query(query, function(err, results){
+        connection.query(query, ids,function(err, results){
            if(err){
                console.log(err);
                return next("Mysql error, check your query");
@@ -46,24 +50,22 @@ app.get(url,function(req, res, next){
             console.log(results);
             
         });
-        
-        
+
     });
-    
-    
+
 });
 
-app.get( "/service/person_table/:id",function(req, res, next){
+app.get( "/service/person_table/:personid",function(req, res, next){
    //arrays to store dynamic parameters
-    var inventory_type_id =[];
+    var ids =[];
     var id=req.params.id;
-    inventory_type_id.push(inventory_type_id);
+    ids.push(id);
     
-    var query = "SELECT * FROM person_table WHERE id = ?";
+    var query = "SELECT * FROM person_table WHERE personid = ?";
     req.getConnection(function(err, connection){
        if(err) return next(err);
         
-        connection.query(query, id, function(err, results){
+        connection.query(query, ids, function(err, results){
            if(err){
                console.log(err);
                return next("Mysql error, check your query");
@@ -71,67 +73,61 @@ app.get( "/service/person_table/:id",function(req, res, next){
             res.json(results);
             
         });
-        
-        
+
     });
-    
-    
+
 });
 
-
-
-app.get("/service/inventory",function(req, res, next){
-   //arrays to store dynamic parameters
-    
-    
-    var query = "SELECT * FROM inventory";
-    req.getConnection(function(err, connection){
-       if(err) return next(err);
-        
-        connection.query(query, function(err, results){
-           if(err){
-               console.log(err);
-               return next("Mysql error, check your query");
-           } 
-            res.json(results);
-            console.log(results);
-            
-        });
-        
-        
-    });
-    
-    
-});
-
-app.get( "/service/inventory/:inventorytype_id",function(req, res, next){
-   //arrays to store dynamic parameters
-    var inventorytype_ids =[];
-    var inventorytype_id=req.params.inventorytype_id;
-    inventorytype_ids.push(inventorytype_id);
-    
-    var query = "SELECT * FROM inventory WHERE inventorytype_id = ?";
-    req.getConnection(function(err, connection){
-       if(err) return next(err);
-        
-        connection.query(query, inventorytype_ids, function(err, results){
-           if(err){
-               console.log(err);
-               return next("Mysql error, check your query");
-           } 
-            res.json(results);
-            
-        });
-        
-        
-    });
-    
-    
-});
-
-
-
-
+//
+//
+// app.get("/service/inventory",function(req, res, next){
+//    //arrays to store dynamic parameters
+//
+//
+//     var query = "SELECT * FROM inventory";
+//     req.getConnection(function(err, connection){
+//        if(err) return next(err);
+//
+//         connection.query(query, function(err, results){
+//            if(err){
+//                console.log(err);
+//                return next("Mysql error, check your query");
+//            }
+//             res.json(results);
+//             console.log(results);
+//
+//         });
+//
+//
+//     });
+//
+//
+// });
+//
+// app.get( "/service/inventory/:inventorytype_id",function(req, res, next){
+//    //arrays to store dynamic parameters
+//     var inventorytype_ids =[];
+//     var inventorytype_id=req.params.inventorytype_id;
+//     inventorytype_ids.push(inventorytype_id);
+//
+//     var query = "SELECT * FROM person_table WHERE inventorytype_id = ?";
+//     req.getConnection(function(err, connection){
+//        if(err) return next(err);
+//
+//         connection.query(query, inventorytype_ids, function(err, results){
+//            if(err){
+//                console.log(err);
+//                return next("Mysql error, check your query");
+//            }
+//             res.json(results);
+//
+//         });
+//
+//
+//     });
+//
+//
+// });
 
 //routing
 app.get('/index', function(req, res){
@@ -141,7 +137,7 @@ app.get('/index', function(req, res){
 //End of routing
 
 //launching application on localhost.
-app.listen(8282, function(){
-   console.log('server loaded on port 8282'); 
+app.listen(3000, function(){
+   console.log('server loaded on port 3000');
     
 });
